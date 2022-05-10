8b2035ad2973b1f12cdd8178480157143e3a97fe
kanazawa keima <little-green7730@softbank.ne.jp>
2022-05-10 17:38:08 +0900

    System Specで任意のタスク詳細画面に遷移したとき、該当タスクの内容が表示されるようにしました

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in'task[title]',with: '金澤'
+        fill_in'task[content]',with: '金澤'
+        click_on 'Create Task'
+      expect(page).to have_content'金澤'
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
+        task = FactoryBot.create(:task, title: 'task', content: 'task')
         visit tasks_path
-        binding.irb
         current_path
         Task.count
         page.html
@@ -22,7 +31,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-
+         @task = FactoryBot.create(:task, title: '成功', content: '成功')
+         visit task_path(@task.id)
+         expect(page).to have_content '成功'
        end
      end
   end
