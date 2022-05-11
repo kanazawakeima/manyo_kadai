288b99407998e84243c7f40866a4f9a6bd743998
kanazawa keima <little-green7730@softbank.ne.jp>
2022-05-11 13:59:58 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ Bundler.require(*Rails.groups)
 module ManyoApp
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     config.generators do |g|
           g.test_framework :rspec,
                        model_specs: true,
